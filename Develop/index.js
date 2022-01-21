d085a5820aebaf37fe4df12919f58b508a478daf
TC <teresa.j.campbell@gmail.com>
2022-01-21 09:04:37 -0500

    added all needed modules

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,10 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
+
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateReadme = require("./utils/generateReadme")
+const writeFileAsync = util.promisify(fs.writeFile);
 // TODO: Create an array of questions for user input
 const questions = [];
 
